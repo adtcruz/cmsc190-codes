fe865d43d608697f5ef07332b53cc5be2fee959a
Anton Dominique Cruz <adtcrz@gmail.com>
2016-11-02 20:29:38 +0800

    Fixed trial script for Objective-C programs

--- objc_trial_10.sh
+++ objc_trial_10.sh
@@ -1,20 +1,11 @@
-gcc -Wall -Werror -g -v 10_numbers.m -framework Foundation -lobjc -o 10_numbers
+gcc -Wall -Werror -g -v 10_numbers.m -framework Foundation -framework QuartzCore -lobjc -o 10_numbers
 ./10_numbers
-gcc -Wall -Werror -g -v 10_numbers.m -framework Foundation -lobjc -o 10_numbers
 ./10_numbers
-gcc -Wall -Werror -g -v 10_numbers.m -framework Foundation -lobjc -o 10_numbers
 ./10_numbers
-gcc -Wall -Werror -g -v 10_numbers.m -framework Foundation -lobjc -o 10_numbers
 ./10_numbers
-gcc -Wall -Werror -g -v 10_numbers.m -framework Foundation -lobjc -o 10_numbers
 ./10_numbers
-gcc -Wall -Werror -g -v 10_numbers.m -framework Foundation -lobjc -o 10_numbers
 ./10_numbers
-gcc -Wall -Werror -g -v 10_numbers.m -framework Foundation -lobjc -o 10_numbers
 ./10_numbers
-gcc -Wall -Werror -g -v 10_numbers.m -framework Foundation -lobjc -o 10_numbers
 ./10_numbers
-gcc -Wall -Werror -g -v 10_numbers.m -framework Foundation -lobjc -o 10_numbers
 ./10_numbers
-gcc -Wall -Werror -g -v 10_numbers.m -framework Foundation -lobjc -o 10_numbers
 ./10_numbers
