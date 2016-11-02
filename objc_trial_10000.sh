5665d80a16b2fee3e845cbba225bccb6946328f2
Anton Dominique Cruz <adtcrz@gmail.com>
2016-11-02 20:57:19 +0800

    Added script for Objective-C 10000 numbers trial

---
+++ objc_trial_10000.sh
@@ -0,0 +1,11 @@
+gcc -Wall -Werror -g -v 10000_numbers.m -framework Foundation -framework QuartzCore -lobjc -o 10000_numbers
+./10000_numbers
+./10000_numbers
+./10000_numbers
+./10000_numbers
+./10000_numbers
+./10000_numbers
+./10000_numbers
+./10000_numbers
+./10000_numbers
+./10000_numbers
