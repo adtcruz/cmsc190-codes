34a430bcec9691a2aab441828eeed84bfc912e4c
Anton Dominique Cruz <adtcrz@gmail.com>
2016-11-02 20:30:37 +0800

    Working Objective-C code

--- 10_numbers.m
+++ 10_numbers.m
@@ -6,22 +6,20 @@ int main(){
   int a_size = ((sizeof nums)/(sizeof nums[0]));
   int i = 0;
   int j = 0;
-  CFTimeInterval startTime = CACurrentMediaTime();
+
+  CFTimeInterval start = CACurrentMediaTime();
+
   for(i=0;i<a_size-1;i++){
-    
-  }
-  /*
-
-  for i in 1...nums.count-1{
-      var temp = nums[i]
-      j = i - 1
-      while (j >= 0 && nums[j] > temp){
-          nums[j+1] = nums[j]
-          nums[j] = temp
-          j = j - 1
-      }
+    int temp = nums[i];
+    j = i - 1;
+    while(j >= 0 && nums[j] > temp){
+      nums[j+1] = nums[i];
+      nums[j] = temp;
+      j = j - 1;
+    }
   }
 
-  print("Execution completed in \(CACurrentMediaTime() - start) seconds")
-  */
+  CFTimeInterval e_time = (CACurrentMediaTime() - start);
+
+  printf("Execution completed %lf seconds\n", e_time);
 }
