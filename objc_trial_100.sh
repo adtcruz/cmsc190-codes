2b693e865c5cce6bd321e8432528bdd09707fa33
Anton Dominique Cruz <adtcrz@gmail.com>
2016-11-02 20:33:40 +0800

    Added script for Objective-C 100 numbers trials

---
+++ objc_trial_100.sh
@@ -0,0 +1,11 @@
+gcc -Wall -Werror -g -v 100_numbers.m -framework Foundation -framework QuartzCore -lobjc -o 100_numbers
+./100_numbers
+./100_numbers
+./100_numbers
+./100_numbers
+./100_numbers
+./100_numbers
+./100_numbers
+./100_numbers
+./100_numbers
+./100_numbers
