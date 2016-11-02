c666c4dfdea6f3df1eed361dd9d46815231cba81
Anton Dominique Cruz <adtcrz@gmail.com>
2016-11-02 20:55:49 +0800

    Added script for 1000 numbers trial

---
+++ objc_trial_1000.sh
@@ -0,0 +1,11 @@
+gcc -Wall -Werror -g -v 1000_numbers.m -framework Foundation -framework QuartzCore -lobjc -o 1000_numbers
+./1000_numbers
+./1000_numbers
+./1000_numbers
+./1000_numbers
+./1000_numbers
+./1000_numbers
+./1000_numbers
+./1000_numbers
+./1000_numbers
+./1000_numbers
