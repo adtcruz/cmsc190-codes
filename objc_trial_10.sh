068bd1710afa6dee4b4118d8c1aee22cc3137ab2
Anton Dominique Cruz <adtcrz@gmail.com>
2016-11-02 20:06:47 +0800

    Added script for Objective-C code runs

---
+++ objc_trial_10.sh
@@ -0,0 +1,20 @@
+gcc -Wall -Werror -g -v 10_numbers.m -framework Foundation -lobjc -o 10_numbers
+./10_numbers
+gcc -Wall -Werror -g -v 10_numbers.m -framework Foundation -lobjc -o 10_numbers
+./10_numbers
+gcc -Wall -Werror -g -v 10_numbers.m -framework Foundation -lobjc -o 10_numbers
+./10_numbers
+gcc -Wall -Werror -g -v 10_numbers.m -framework Foundation -lobjc -o 10_numbers
+./10_numbers
+gcc -Wall -Werror -g -v 10_numbers.m -framework Foundation -lobjc -o 10_numbers
+./10_numbers
+gcc -Wall -Werror -g -v 10_numbers.m -framework Foundation -lobjc -o 10_numbers
+./10_numbers
+gcc -Wall -Werror -g -v 10_numbers.m -framework Foundation -lobjc -o 10_numbers
+./10_numbers
+gcc -Wall -Werror -g -v 10_numbers.m -framework Foundation -lobjc -o 10_numbers
+./10_numbers
+gcc -Wall -Werror -g -v 10_numbers.m -framework Foundation -lobjc -o 10_numbers
+./10_numbers
+gcc -Wall -Werror -g -v 10_numbers.m -framework Foundation -lobjc -o 10_numbers
+./10_numbers
