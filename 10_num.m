07eb8bd88b4532ed08f6d26baed90dc5c3ed5d29
Anton Dominique Cruz <adtcrz@gmail.com>
2016-11-02 20:02:41 +0800

    Added Objective-C code sorting 10 numbers

---
+++ 10_num.m
@@ -0,0 +1,23 @@
+#import <Foundation/Foundation.h>
+
+int main(){
+  int nums[] = {10, 1, 4, 3, 2, 9, 5, 6, 8, 7};
+  int a_size = ((sizeof nums)/(sizeof nums[0]));
+  int i = 0;
+  int j = 0;
+  /*
+  let start: TimeInterval = CACurrentMediaTime()
+
+  for i in 1...nums.count-1{
+      var temp = nums[i]
+      j = i - 1
+      while (j >= 0 && nums[j] > temp){
+          nums[j+1] = nums[j]
+          nums[j] = temp
+          j = j - 1
+      }
+  }
+
+  print("Execution completed in \(CACurrentMediaTime() - start) seconds")
+  */
+}
