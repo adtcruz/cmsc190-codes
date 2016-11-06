0bf4f902806d975e69e9c4c265e7f4ab8e43a7ab
Anton Dominique Cruz <adtcrz@gmail.com>
2016-11-06 13:22:56 +0800

    Explicitly declaring variables as integers

--- 10_numbers.swift
+++ 10_numbers.swift
@@ -1,15 +1,15 @@
 import Foundation
 import QuartzCore
 
-var nums = [10, 1, 4, 3, 2, 9, 5, 6, 8, 7]
+var nums:[Int] = [10, 1, 4, 3, 2, 9, 5, 6, 8, 7]
 
-var i = 0;
-var j = 0;
+var i:Int = 0;
+var j:Int = 0;
 
 let start: TimeInterval = CACurrentMediaTime()
 
 for i in 1...nums.count-1{
-    var temp = nums[i]
+    var temp:Int = nums[i]
     j = i - 1
     while (j >= 0 && nums[j] > temp){
         nums[j+1] = nums[j]
