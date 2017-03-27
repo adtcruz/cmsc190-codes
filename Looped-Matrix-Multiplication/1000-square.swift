d64f568049ed66502310b03d56186422f6e29a93
Anton Dominique Cruz <adtcrz@gmail.com>
2017-03-27 20:57:31 +0800

    Modified programs

--- Looped-Matrix-Multiplication/1000-square.swift
+++ Looped-Matrix-Multiplication/1000-square.swift
@@ -1,11 +1,12 @@
 import Foundation
 import QuartzCore
 
-var dimensionSize = 1000
+var dimensionN = 1000
+var dimensionM = 1000
 
-var mat1:[[Int]] = [[Int]](repeating:[Int](repeating:0,count:dimensionSize),count:dimensionSize)
-var mat2:[[Int]] = [[Int]](repeating:[Int](repeating:0,count:dimensionSize),count:dimensionSize)
-var result:[[Int]] = [[Int]](repeating:[Int](repeating:0,count:dimensionSize),count:dimensionSize)
+var mat1:[[Int]] = [[Int]](repeating:[Int](repeating:0,count:dimensionM),count:dimensionN)
+var mat2:[[Int]] = [[Int]](repeating:[Int](repeating:0,count:dimensionN),count:dimensionM)
+var result:[[Int]] = [[Int]](repeating:[Int](repeating:0,count:dimensionN),count:dimensionN)
 
 var i:Int = 0
 var j:Int = 0
@@ -13,15 +14,21 @@ var k:Int = 0
 var l:Int = 0
 
 //randomise content
-for i in 0...(dimensionSize - 1){
-  for j in 0...(dimensionSize - 1){
+for i in 0...(dimensionN - 1){
+  for j in 0...(dimensionM - 1){
 
     mat1[i][j] = Int(arc4random_uniform(UInt32(10001)))
-    mat2[i][j] = Int(arc4random_uniform(UInt32(10001)))
 
     if(arc4random()%2 == 1){
       mat1[i][j] = mat1[i][j] * -1
     }
+  }
+}
+
+for i in 0...(dimensionM - 1){
+  for j in 0...(dimensionN - 1){
+
+    mat2[i][j] = Int(arc4random_uniform(UInt32(10001)))
 
     if(arc4random()%2 == 1){
       mat2[i][j] = mat2[i][j] * -1
@@ -31,12 +38,12 @@ for i in 0...(dimensionSize - 1){
 
 let start: TimeInterval = CACurrentMediaTime()
 
-for i in 0...(dimensionSize - 1){
-  for j in 0...(dimensionSize - 1){
+for i in 0...(dimensionN - 1){
+  for j in 0...(dimensionN - 1){
 
     var partProd:Int = 0
 
-    for k in 0...(dimensionSize - 1){
+    for k in 0...(dimensionM - 1){
       partProd = (mat1[i][k] * mat2[k][j]) + partProd
     }
 
